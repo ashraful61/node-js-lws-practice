744decf349422443c7d566a28640ffaa7c940397
ashraf_bmqa <ashraful.islam@bengalmobileqa.com>
2023-01-21 21:30:21 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const todoHandler = require("./routeHandler/todoHandler");
+const userHandler = require("./routeHandler/userHandler")
+
+const port = process.env.PORT || 5000;
+
+// express app initialization
+const app = express();
+require('dotenv').config();
+app.use(express.json());
+
+// database connection with mongoose
+mongoose.set('strictQuery', false);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clustertestashraf.z94m9ys.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(uri, {
+    dbName: 'TodoDb',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connection successful"))
+  .catch((err) => console.log(err));
+
+// Application Routes
+//Todo Route
+app.use("/todo", todoHandler);
+
+//User Route
+app.use("/user", userHandler)
+
+// default error handler
+function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).json({ error: err });
+}
+
+app.listen(port, () => {
+  console.log(`app listening at port: ${port}`);
+});
---
+++ routeHandler/todoHandler.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const todoSchema = require("../schemas/todoSchema");
+const Todo = new mongoose.model("Todo", todoSchema);
+
+//Get all the todo
+router.get("/", async (req, res) => {
+  try {
+    // const newTodo = new Todo(req.body);
+    const allTodo = await Todo.find({});
+    res.status(200).json({
+      status: true,
+      resultSet: allTodo,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: "There was a server side error",
+      status: false,
+    });
+  }
+});
+
+//Get todo by id
+router.get("/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findOne({ _id: req.params.id });
+    res.status(200).json({
+      status: true,
+      result: todo,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: "There was a server side error",
+      status: false,
+    });
+  }
+});
+
+//Post a todo
+router.post("/", async (req, res) => {
+  try {
+    const newTodo = new Todo(req.body);
+    await newTodo.save();
+    res.status(200).json({
+      status: true,
+      message: "Todo was inserted successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: "There was a server side error",
+      status: false,
+    });
+  }
+});
+
+//Get all todo
+router.post("/all", async (req, res) => {
+  try {
+    await Todo.insertMany(req.body);
+    res.status(200).json({
+      status: true,
+      message: "Todo's were inserted successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: "There was a server side error",
+      status: false,
+    });
+  }
+});
+
+//Update a todo
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedDoc = await Todo.findByIdAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          status: "inactive",
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    console.log(updatedDoc);
+    res.status(200).json({
+      status: true,
+      message: "Todo was updated successfully",
+      result: updatedDoc,
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "There was a server side error", status: false });
+  }
+});
+
+//Delete a todo
+router.delete("/:id", async (req, res) => {
+  try {
+    const todo = await Todo.deleteOne({ _id: req.params.id });
+    res.status(200).json({
+      status: true,
+      message: "Deleted Successful",
+      result: todo,
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "There was a server side error", status: false });
+  }
+});
+
+module.exports = router;
