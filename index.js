65a7e5579efe5212cc10e736e97ac366ad2874f7
ashraful61 <ashraful.ru37@gmail.com>
2023-03-13 18:40:52 +0600

    added some code for query

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const todoHandler = require("./routeHandler/todoHandler");
-const userHandler = require("./routeHandler/userHandler")
+const userHandler = require("./routeHandler/userHandler");
 
 const port = process.env.PORT || 5000;
 
 // express app initialization
 const app = express();
-require('dotenv').config();
+const dotenv = require("dotenv").config();
 app.use(express.json());
 
 // database connection with mongoose
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clustertestashraf.z94m9ys.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose
   .connect(uri, {
-    dbName: 'TodoDb',
+    dbName: "Todo",
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("connection successful"))
+  .then(() => console.log("db connection successful"))
   .catch((err) => console.log(err));
 
 // Application Routes
 //Todo Route
 app.use("/todo", todoHandler);
-
 //User Route
-app.use("/user", userHandler)
+app.use("/user", userHandler);
 
 // default error handler
-const errorHandler =  (err, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
   }
   res.status(500).json({ error: err });
-}
+};
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`app listening at port: ${port}`);
--- middleWares/checkLogin.js
+++ middleWares/checkLogin.js
@@ -1,14 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const checkLogin = async (req, res, next) => {
-  const { authorization } = req.headers;
+  
   try {
-    const token = authorization.split(' ')[1];
-    const decoded = await jwt.verify(token, process.env.JWT_SECRET_KEY);
-    const { username, userId } = decoded;
-    req.username = username;
-    req.userId = userId;
-    next();
+    const { authorization } = req.headers;
+    if(authorization){
+      const token = authorization.split(' ')[1];
+      const decoded = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+      const { username, userId } = decoded;
+      req.username = username;
+      req.userId = userId;
+      next();
+    }
+    else {
+      next("Authentication error!")
+    }
   } catch(err) {
     console.log(err)
     next("Authentication error!");
